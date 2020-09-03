aed04e013fa6e26c60492b50e25647a9137d5eea
Shelby <Shelby@192.168.0.9>
2020-09-03 15:06:37 -0400

    Pvp kit

--- TestPvP/src/Kits/pvp.java
+++ TestPvP/src/Kits/pvp.java
@@ -2,11 +2,13 @@ package Kits;
 
 import org.bukkit.Material;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class pvp 
+  implements CommandExecutor 
 {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
--- TestPvP/src/PvP/Pvpmain.java
+++ TestPvP/src/PvP/Pvpmain.java
@@ -3,12 +3,16 @@ package PvP;
 import org.bukkit.Color;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import Kits.pvp;
+
 public class Pvpmain extends JavaPlugin
 {
 	@Override
 	public void onEnable()
 	{
 		getLogger().info(Color.RED + "PvP has been enabled!");
+		
+		this.getCommand("pvp").setExecutor(new pvp());
 	}
 	
 	@Override
